fdec3a81bd4c82e55468cee18fcbbc4608617d6a
eightfeet <bajiao000000@163.com>
2023-09-04 07:22:53 +0800

    publish demo

---
+++ assets/index-18cbdbf8.js
@@ -0,0 +1,1 @@
+import{j as e,L as s}from"./index-d115b89f.js";import{u as t,S as i}from"./useDocumentTitle-cf41a056.js";const r=({name:o="home"})=>(t(o),e.jsx("div",{children:e.jsxs(i,{children:[e.jsx(s,{to:"/",children:"首页"}),e.jsx(s,{to:"/list",children:"列表"}),e.jsx(s,{to:"/view",children:"详情"})]})})),m=r;export{m as default};
