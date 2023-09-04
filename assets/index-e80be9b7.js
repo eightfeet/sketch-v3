0b57415bc6c6739b4729c43d208099b6fee6225f
eightfeet <bajiao000000@163.com>
2023-09-04 23:30:22 +0800

    publish demo

---
+++ assets/index-e80be9b7.js
@@ -0,0 +1,1 @@
+import{j as e,L as s}from"./index-557c12ea.js";import{u as t,S as i}from"./useDocumentTitle-5a665c52.js";const r=({name:o="home"})=>(t(o),e.jsx("div",{children:e.jsxs(i,{children:[e.jsx(s,{to:"/",children:"首页"}),e.jsx(s,{to:"/list",children:"列表"}),e.jsx(s,{to:"/view",children:"详情"})]})})),m=r;export{m as default};
